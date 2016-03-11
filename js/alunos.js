81776f99fb0fda402eae459428e401c6a5cd3e71
eliassoares19 <eliassoares19@gmail.com>
2016-03-11 17:20:36 -0300

    novo commit do arquivo

--- js/alunos.js
+++ js/alunos.js
@@ -1,12 +1,12 @@
 var alunos = [
-    /*{
+    {
         "nome": "wladek zacharski dos Santos",
         "curso": "Ciência da Computação",
         "mensagem": "Ta legal!"
     },
     {
-        "nome": "Fulando de tal",
-        "curso": "Outra coisas",
+        "nome": "Elias Soares",
+        "curso": "Ciência da Computação",
         "mensagem": "Muito show"
-    }*/
+    }
 ];
